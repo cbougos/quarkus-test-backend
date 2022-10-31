b8cba8a8b2a7cf2ead61a5bf7ff46395866ef6e0
cbougos <akisbougos@hotmail.com>
2022-10-31 12:06:22 +0200

    Dockerfile updates

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,16 +1,21 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
+@Slf4j
 public class ExampleResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        log.info("Log from hello resource!");
+        return Response.ok("Hello from RESTEasy Reactive!").build();
     }
 }
